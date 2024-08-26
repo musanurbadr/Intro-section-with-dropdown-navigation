bb192e2d8de37fa3081cfff6a42563e77f6e6706
musanurbadr <111696681+musanurbadr@users.noreply.github.com>
2024-08-26 22:21:13 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
+// tailwind.config.js
 module.exports = {
-  content: ['./*.html'],
+  content: [
+    './*.html',      // HTML dosyalarını tarar
+    './src/**/*.{js,jsx,ts,tsx}', // JS, JSX, TS ve TSX dosyalarını tarar (eğer varsa)
+  ],
   theme: {
-    maxWidth: {
-      custom: '1100px',
+    extend: {
+      maxWidth: {
+        'custom': '1100px',
+      },
+      colors: {
+        'almost-white': 'hsl(0, 0%, 98%)',
+        'medium-gray': 'hsl(0, 0%, 41%)',
+        'almost-black': 'hsl(0, 0%, 8%)',
+      },
+      screens: {
+        'mdd': {'max': '800px'},
+      },
     },
     container: {
-      center :true
+      center: true,
     },
-    colors: {
-      AlmostWhite:"hsl(0, 0%, 98%)",
-      MediumGray:"hsl(0, 0%, 41%)",
-      AlmostBlack:"hsl(0, 0%, 8%)",
-    },
-    screens: {
-      md: {'max': '800px'},
-    },
-    extend: {},
   },
   plugins: [],
 }
-
